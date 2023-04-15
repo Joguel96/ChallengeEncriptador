3b0f331d2c10deaeed402c49fd5b6b9d753fa3b4
joguel96 <joguel96@gmail.com>
2023-04-14 18:17:05 -0700

    Update
    Se actualiza la organizacion de los elementos en el HTML, se actualizan y mejoran los estilos CSS y se amplian las funciones del script para capturar mas validaciones

--- script.js
+++ script.js
@@ -15,27 +15,46 @@ function ajustarAlturaTextArea(textArea) {
 
 function encriptar() {
     mensaje = document.getElementById("mensaje").value;
-    const mensajeEncriptado = mensaje
-        .replace(/e/g, "enter")
-        .replace(/i/g, "imes")
-        .replace(/a/g, "ai")
-        .replace(/o/g, "ober")
-        .replace(/u/g, "ufat");
+    let  mensajeEncriptado;
 
-        document.getElementById("resultado").innerHTML = mensajeEncriptado;
-        return mensajeEncriptado;
+    mensaje ? (() => {
+      mensajeEncriptado = mensaje
+      .replace(/e/g, "enter")
+      .replace(/i/g, "imes")
+      .replace(/a/g, "ai")
+      .replace(/o/g, "ober")
+      .replace(/u/g, "ufat");
+      
+      document.getElementById("resultado").innerHTML = mensajeEncriptado;
+      document.getElementById("mensaje").value = "";
+
+    }) () : (() => {
+      notifica("No existe ningun mensaje");
+
+    })();
+    return mensajeEncriptado;
+
 }
 
 function desencriptar() {
     mensaje = document.getElementById("mensaje").value;
-    const mensajeDesencriptado = mensaje
-        .replace(/enter/g, "e")
-        .replace(/imes/g, "i")
-        .replace(/ai/g, "a")
-        .replace(/ober/g, "o")
-        .replace(/ufat/g, "u");
+    let mensajeDesencriptado;
 
-        document.getElementById("resultado").innerHTML = mensajeDesencriptado;
+    mensaje ? (() => { 
+
+      mensajeDesencriptado = mensaje
+      .replace(/enter/g, "e")
+      .replace(/imes/g, "i")
+      .replace(/ai/g, "a")
+      .replace(/ober/g, "o")
+      .replace(/ufat/g, "u");
+      
+      document.getElementById("resultado").innerHTML = mensajeDesencriptado;
+      document.getElementById("mensaje").value = "";
+    }) () : (() => {
+      notifica("No existe ningun mensaje");
+
+    })();
         return mensajeDesencriptado;
 }
 
@@ -43,10 +62,31 @@ function copiar() {
     const mensaje = document.getElementById("resultado").textContent;
     navigator.clipboard.writeText(mensaje)
     .then(() => {
-      console.log('resultado copiado al portapapeles: ' + mensaje);
-      document.getElementById("mensaje").value = "";
+      document.getElementById("resultado").textContent = "";
+      mensaje ? notifica("Se copio el texto") : sinRespuesta("hola mundo sin respuesta");
     })
     .catch(err => {
       console.error('No se pudo copiar el resultado al portapapeles: ', err);
     });
 }
+
+
+function notifica(mensaje) {
+  const contenedorNotificacion = document.getElementById("contenedor-notificacion");
+  const notificacion = document.getElementById("notificacion");
+  notificacion.innerHTML = mensaje;
+  contenedorNotificacion.style.display = "block";
+  contenedorNotificacion.style.animation = "slide-down 0.5s ease-in-out forwards";
+
+  setTimeout(function() {
+    contenedorNotificacion.style.animation = "fade-out 0.5s ease-in-out forwards";
+
+    setTimeout(function() {
+      contenedorNotificacion.style.display = "none";
+    }, 500);
+  }, 1500);
+}
+
+  function sinRespuesta(mensaje){
+    console.log(mensaje)
+  }
